9625b8c4891861fd163dc9b58b479a32c7f475e9
BuraYu <burak.yueksel@hotmail.com>
2025-04-18 18:27:57 +0200

    feat: add GET req in events API

--- src/app/api/events/route.js
+++ src/app/api/events/route.js
@@ -1,14 +1,22 @@
 import { connectToDatabase } from "@/lib/mongodb";
 import Event from "@/models/Event";
 
-export async function GET(req) {
+export async function GET(req, res) {
+  const { method } = req;
+
   await connectToDatabase();
-  try {
-    const events = await Event.find(); 
-    return new Response(JSON.stringify(events), { status: 200 });
-  } catch (error) {
-    return new Response(JSON.stringify({ error: "Failed to fetch events" }), {
-      status: 500,
-    });
+
+  if (method == "GET") {
+    try {
+      const events = await Event.find();
+      return new Response(JSON.stringify(events), { status: 200 });
+    } catch (error) {
+      return new Response(JSON.stringify({ error: "Failed to fetch events" }), {
+        status: 500,
+      });
+    }
+  } else {
+    res.setHeader("Allow", ["GET"]);
+    res.status(405).end(`Method ${method} not implemeted yet`);
   }
 }
