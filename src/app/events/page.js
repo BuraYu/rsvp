80bf20b334b92737efdc7e460f32dd4b15a9de92
BuraYu <burak.yueksel@hotmail.com>
2025-04-18 20:18:20 +0200

    feat: filter of online/offline events

--- src/app/events/page.js
+++ src/app/events/page.js
@@ -18,6 +18,8 @@ export default function Events() {
   const [activeButton, setActiveButton] = useState("All");
   const [events, setEvents] = useState([]);
   const [filteredEvents, setFilteredEvents] = useState([]);
+  const [onlineEvents, setOnlineEvents] = useState([]);
+  const [offlineEvents, setOfflineEvents] = useState([]);
 
   useEffect(() => {
     const fetchEvents = async () => {
@@ -42,12 +44,30 @@ export default function Events() {
     fetchEvents();
   }, []);
 
+  useEffect(() => {
+    const separateEvents = (events) => {
+      const online = events.filter((event) => event.mode === "online");
+      const offline = events.filter((event) => event.mode === "offline");
+
+      setOnlineEvents(online);
+      setOfflineEvents(offline);
+    };
+
+    separateEvents(events);
+  }, [events]);
+
   const handleButtonClick = (category) => {
     setActiveButton(category);
-    if (category == "All") {
+
+    if (category === "All") {
       setFilteredEvents(events);
+      setOnlineEvents(events.filter((event) => event.mode === "online"));
+      setOfflineEvents(events.filter((event) => event.mode === "offline"));
     } else {
-      setFilteredEvents(events.filter((event) => event.category === category));
+      const filtered = events.filter((event) => event.category === category);
+      setFilteredEvents(filtered);
+      setOnlineEvents(filtered.filter((event) => event.mode === "online"));
+      setOfflineEvents(filtered.filter((event) => event.mode === "offline"));
     }
   };
 
@@ -108,7 +128,7 @@ export default function Events() {
               align: "start",
               loop: true,
             }}
-            className="relative w-full"
+            className="relative w-full mb-10"
           >
             {/* Arrows */}
             <div className="absolute -top-6 right-15 md:right-25 lg:right-19 xl:right-15 2xl:right-18 z-10 flex space-x-2">
@@ -159,11 +179,120 @@ export default function Events() {
             </CarouselContent>
           </Carousel>
 
-          <div></div>
           {/* Offline events */}
-          <h2 className="text-3xl font-bold">Offline Events</h2>
+          <h2 className="text-3xl font-bold mb-3">Offline Events</h2>
+          <Carousel
+            opts={{
+              align: "start",
+              loop: true,
+            }}
+            className="relative w-full mb-10"
+          >
+            {/* Arrows */}
+            <div className="absolute -top-6 right-15 md:right-25 lg:right-19 xl:right-15 2xl:right-18 z-10 flex space-x-2">
+              <CarouselPrevious className="text-pink-500 hover:text-pink-700 hover:scale-110 transition-transform duration-200" />
+              <CarouselNext className="text-pink-500 hover:text-pink-700 hover:scale-110 transition-transform duration-200" />
+            </div>
+
+            {/* Carousel Items */}
+            <CarouselContent className="flex gap-4 px-2">
+              {offlineEvents.map((event, index) => (
+                <CarouselItem
+                  key={index}
+                  className="flex flex-shrink-0 md:basis-1/2 lg:basis-1/3 xl:basis-1/4"
+                >
+                  <Card className="w-full md:w-[85%]">
+                    <CardContent className="relative flex h-auto p-4">
+                      <div className="flex flex-row md:flex-col space-y-2 w-full">
+                        <div className="w-1/2 md:w-full">
+                          <Image
+                            src="https://picsum.photos/350/250"
+                            width={350}
+                            height={250}
+                            alt="Picture of the author"
+                            className="w-full h-auto"
+                          />
+                        </div>
+                        <div className="w-1/2 md:w-full flex flex-col justify-center gap-1 sm:ml-5 md:ml-0">
+                          <span className="text-[0.7rem] text-gray-500 mt-1">
+                            {formatDate(event.dateTime)}
+                          </span>{" "}
+                          <h3 className="text-2xl font-bold mb-4">
+                            {event.name}
+                          </h3>
+                          <span
+                            className={`inline-block self-start text-[0.6rem] rounded-md px-2 py-1 ${getCategoryColor(
+                              event.category
+                            )}`}
+                          >
+                            {" "}
+                            {event.category}
+                          </span>
+                        </div>
+                      </div>
+                    </CardContent>
+                  </Card>
+                </CarouselItem>
+              ))}
+            </CarouselContent>
+          </Carousel>
           {/* Online events */}
-          <h2 className="text-3xl font-bold">Online Events</h2>
+          <h2 className="text-3xl font-bold mb-3">Online Events</h2>
+          <Carousel
+            opts={{
+              align: "start",
+              loop: true,
+            }}
+            className="relative w-full"
+          >
+            {/* Arrows */}
+            <div className="absolute -top-6 right-15 md:right-25 lg:right-19 xl:right-15 2xl:right-18 z-10 flex space-x-2">
+              <CarouselPrevious className="text-pink-500 hover:text-pink-700 hover:scale-110 transition-transform duration-200" />
+              <CarouselNext className="text-pink-500 hover:text-pink-700 hover:scale-110 transition-transform duration-200" />
+            </div>
+
+            {/* Carousel Items */}
+            <CarouselContent className="flex gap-4 px-2">
+              {onlineEvents.map((event, index) => (
+                <CarouselItem
+                  key={index}
+                  className="flex flex-shrink-0 md:basis-1/2 lg:basis-1/3 xl:basis-1/4"
+                >
+                  <Card className="w-full md:w-[85%]">
+                    <CardContent className="relative flex h-auto p-4">
+                      <div className="flex flex-row md:flex-col space-y-2 w-full">
+                        <div className="w-1/2 md:w-full">
+                          <Image
+                            src="https://picsum.photos/350/250"
+                            width={350}
+                            height={250}
+                            alt="Picture of the author"
+                            className="w-full h-auto"
+                          />
+                        </div>
+                        <div className="w-1/2 md:w-full flex flex-col justify-center gap-1 sm:ml-5 md:ml-0">
+                          <span className="text-[0.7rem] text-gray-500 mt-1">
+                            {formatDate(event.dateTime)}
+                          </span>{" "}
+                          <h3 className="text-2xl font-bold mb-4">
+                            {event.name}
+                          </h3>
+                          <span
+                            className={`inline-block self-start text-[0.6rem] rounded-md px-2 py-1 ${getCategoryColor(
+                              event.category
+                            )}`}
+                          >
+                            {" "}
+                            {event.category}
+                          </span>
+                        </div>
+                      </div>
+                    </CardContent>
+                  </Card>
+                </CarouselItem>
+              ))}
+            </CarouselContent>
+          </Carousel>
         </div>
       </div>
     </div>
