84c4edb36fe321bdfe7ccbe681a3e83f36abb376
BuraYu <burak.yueksel@hotmail.com>
2025-04-22 20:43:06 +0200

    feat: add context to dashboard

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,7 +1,16 @@
+"use client";
+
+import { useContext } from "react";
+import { AuthContext } from "@/lib/AuthContext";
+
 export default function Dashboard() {
-  return (
+  const { isAuthenticated } = useContext(AuthContext);
+
+  return isAuthenticated ? (
     <div>
       <h1>Welcome to the Dashboard</h1>
     </div>
+  ) : (
+    <h2>Please login to access</h2>
   );
 }
