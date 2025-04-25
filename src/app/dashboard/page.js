dbe687c970ac47263b91ba13d26f5c6d021350b0
BuraYu <burak.yueksel@hotmail.com>
2025-04-25 21:53:37 +0200

    feat: add main page for dashboard

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -12,8 +12,37 @@ export default function Dashboard() {
       <Sidebar />
       <div className="p-4 w-full">
         <h2 className="text-2xl font-bold">Dashboard</h2>
-        <div className="h-full w-full flex justify-center ">
-          test
+        <div className="flex items-center justify-center min-h-screen">
+          <div className="grid grid-flow-col gap-4">
+            {/* Total Events Added */}
+            <div className="bg-white shadow-md rounded-lg p-4 w-[300px] h-[200px] flex flex-col justify-center items-center">
+              <p className="text-5xl font-semibold mb-2">2</p>
+              <h3 className="text-lg font-semibold text-center">
+                Total Events Added
+              </h3>
+            </div>
+
+            <div className="bg-white shadow-md rounded-lg p-4 w-[300px] h-[200px] flex flex-col justify-center items-center">
+              <p className="text-5xl font-semibold mb-2">22</p>
+              <h3 className="text-lg font-semibold text-center">
+                Public Events Added
+              </h3>
+            </div>
+
+            <div className="bg-white shadow-md rounded-lg p-4 w-[300px] h-[200px] flex flex-col justify-center items-center">
+              <p className="text-5xl font-semibold mb-2">12</p>
+              <h3 className="text-lg font-semibold text-center">
+                Private Events Added
+              </h3>
+            </div>
+
+            <div className="bg-white shadow-md rounded-lg p-4 w-[300px] h-[200px] flex flex-col justify-center items-center">
+              <p className="text-5xl font-semibold mb-2">13</p>
+              <h3 className="text-lg font-semibold text-center">
+                Total Attendees
+              </h3>
+            </div>
+          </div>
         </div>
       </div>
     </div>
