fa7eeb39645c0697ca064da0c292adb532e7175b
BuraYu <burak.yueksel@hotmail.com>
2025-04-26 20:26:31 +0200

    fix: logout function

--- src/lib/logout.js
+++ src/lib/logout.js
@@ -1,13 +1,9 @@
 export async function logout() {
-  // Entferne hier deine Tokens oder Session-Daten
   if (typeof window !== "undefined") {
-    // z.B. LocalStorage clearen (je nach Auth-Mechanismus)
     localStorage.removeItem("authToken");
     localStorage.removeItem("username");
 
-    // Optional: Cookie löschen, API-Call, usw.
 
-    // Danach weiterleiten
     window.location.href = "/";
   }
 }
