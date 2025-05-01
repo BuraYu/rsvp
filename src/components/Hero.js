55cc047938ac8d73f2b147dc5e32ff73e29edd28
BuraYu <burak.yueksel@hotmail.com>
2025-05-01 19:01:20 +0200

    feat: add video for hero section, fix responsiveness

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,24 +2,46 @@ import Link from "next/link";
 
 export default function Hero() {
   return (
-    <div className="flex-1 flex flex-col justify-center items-center bg-gray-900 text-white text-center p-4 gap-5">
-      <h1 className="text-7xl font-bold mb-4">
-        Create and find Unforgettable Events
-      </h1>
-      <p className="text-lg mb-6">RSVP and Management App</p>
-      <div className="flex justify-center items-center">
-        <Link
-          href="/dashboard"
-          className="inline-block bg-amber-300 text-gray-900 font-bold py-2 px-4 rounded-lg hover:bg-gray-200 transition mr-[10px]"
+    <div className="relative flex-1 flex flex-col justify-center items-center text-white text-center p-4 gap-5">
+      <video
+        className="absolute top-0 left-0 w-full h-full object-cover z-10 filter blur-sm"
+        src="/assets/event.mp4"
+        autoPlay
+        loop
+        muted
+        playsInline
+        aria-hidden="true"
+      >
+        Your browser does not support the video tag.
+      </video>
+
+      <div className="relative z-10">
+        <h1
+          className="text-3xl md:text-6xl font-bold mb-4 text-shadow-pink"
+          style={{ textShadow: "2px 1px 1px black" }}
         >
-          Go to Dashboard
-        </Link>
-        <Link
-          href="/events"
-          className="inline-block bg-white text-gray-900 font-bold py-2 px-4 rounded-lg hover:bg-gray-200 transition"
+          Create and find Unforgettable Events
+        </h1>
+        <p
+          className="md:text-lg text-accent mb-6 drop-shadow-xl"
+          style={{ textShadow: "2px 1px 1px black" }}
         >
-          Explore events
-        </Link>
+          RSVP and Management App
+        </p>
+        <div className="flex justify-center items-center">
+          <Link
+            href="/dashboard"
+            className="inline-block bg-amber-300 text-gray-700 font-bold md:text-lg text-xs py-3 px-5 rounded-full hover:bg-gray-200 transition mr-[10px]"
+          >
+            Go to Dashboard
+          </Link>
+          <Link
+            href="/events"
+            className="inline-block bg-white text-gray-900 font-bold md:text-lg text-xs py-3 px-5 rounded-full hover:bg-gray-200 transition"
+          >
+            Explore events
+          </Link>
+        </div>
       </div>
     </div>
   );
