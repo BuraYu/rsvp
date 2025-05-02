24add9edd07b0f93543d86cb47fe687825818cee
BuraYu <burak.yueksel@hotmail.com>
2025-05-02 17:52:42 +0200

    style: dashboard main page updated

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -7,48 +7,49 @@ import Sidebar from "@/components/Sidebar";
 export default function Dashboard() {
   const { isAuthenticated } = useContext(AuthContext);
 
+  const data = [
+    { label: "Total Events Added", value: 1, filter: "total" },
+    { label: "Private Events Added", value: 0, filter: "private" },
+    { label: "Public Events Added", value: 1, filter: "public" },
+    { label: "Offline Events Added", value: 1, filter: "offline" },
+    { label: "Online Events Added", value: 0, filter: "online" },
+  ];
+
   return isAuthenticated ? (
     <div className="flex bg-gray-100 h-screen">
       <Sidebar />
-      <div className="p-4 w-full">
+      <div className="flex-1 p-8 overflow-auto">
         <h2 className="text-3xl font-bold text-gray-800 mb-4 text-center">
           Dashboard
         </h2>
-        <div className="flex items-center justify-center min-h-screen">
-          <div className="grid grid-flow-col gap-4">
-            {/* Total Events Added */}
-            <div className="bg-white shadow-md rounded-lg p-4 w-[300px] h-[200px] flex flex-col justify-center items-center">
-              <p className="text-5xl font-semibold mb-2">2</p>
-              <h3 className="text-lg font-semibold text-center">
-                Total Events Added
-              </h3>
-            </div>
 
-            <div className="bg-white shadow-md rounded-lg p-4 w-[300px] h-[200px] flex flex-col justify-center items-center">
-              <p className="text-5xl font-semibold mb-2">22</p>
-              <h3 className="text-lg font-semibold text-center">
-                Public Events Added
-              </h3>
-            </div>
+        {/* Event Information */}
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-6">
+          {data.map((item) => (
+            <div
+              key={item.filter}
+              className="flex flex-col items-center justify-center text-lg p-4 text-center group"
+            >
+              <h2 className="font-bold text-6xl text-black mb-3">
+                {item.value}
+              </h2>
+              <p className="text-neutral-600 text-sm font-poppins mb-3">
+                {item.label}
+              </p>
 
-            <div className="bg-white shadow-md rounded-lg p-4 w-[300px] h-[200px] flex flex-col justify-center items-center">
-              <p className="text-5xl font-semibold mb-2">12</p>
-              <h3 className="text-lg font-semibold text-center">
-                Private Events Added
-              </h3>
+              {/* See All Link */}
+              <a
+                href={`/dashboard/events?filter=${item.filter}`}
+                className="sidebar-link font-sans -translate-y-5 opacity-0 group-hover:translate-y-0 group-hover:opacity-100 transition-all"
+              >
+                See all
+              </a>
             </div>
-
-            <div className="bg-white shadow-md rounded-lg p-4 w-[300px] h-[200px] flex flex-col justify-center items-center">
-              <p className="text-5xl font-semibold mb-2">13</p>
-              <h3 className="text-lg font-semibold text-center">
-                Total Attendees
-              </h3>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
     </div>
   ) : (
-    <h2>Please login to access</h2>
+    <h2 className="text-center text-xl mt-10">Please login to access</h2>
   );
 }
